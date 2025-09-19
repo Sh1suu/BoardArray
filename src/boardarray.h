f66266fbf135cea0ca596af6a97a1b4b97fdf51b
Francis Benedict Gaviola <gaviolabenedict@gmail.com>
2025-09-19 13:32:15 +0800

    added initial changes (WIP)

--- src/boardarray.h
+++ src/boardarray.h
@@ -3,26 +3,53 @@
 #define SIZE 5
 using namespace std;
 
-class BoardArray : public Board {
-    Entry* array;
+class BoardArray : public Board
+{
+    Entry *array;
     int index;
 
-    public:
-        BoardArray() {
-            array = new Entry[SIZE];
-            index = 0;
-        }
+public:
+    BoardArray()
+    {
+        array = new Entry[SIZE];
+        index = 0;
+    }
 
-        void add(Entry* entry) {
-            // TODO: IMPLEMENT THIS FUNCTION
-            // ALGORITHM IS PROVIDED IN INSTRUCTIONS.TXT
-            return;
-        }
+    void add(Entry *entry)
+    {
+        // TODO: IMPLEMENT THIS FUNCTION
+        // ALGORITHM IS PROVIDED IN INSTRUCTIONS.TXT
 
-        void print() {
-            for (int i = 0; i < index; i++) {
-                cout << i + 1 << ". ";
-                array[i].print();
+        int pos = index;
+
+        for (int i = 0; i < index; i++)
+        {
+            if (entry->compare(array[i]))
+            {
+                pos = i;
+                break;
             }
         }
+
+        for (int i = index; i > pos; i--) { // shift right
+            array[i] = array[i - 1];
+        }
+        array[pos] = *entry;
+        index++;
+
+        if (index == SIZE && pos == index)
+        {
+            cout << entry->name << "'s score is too low to be added!" << endl;
+            return;
+        }
+    }
+
+    void print()
+    {
+        for (int i = 0; i < index; i++)
+        {
+            cout << i + 1 << ". ";
+            array[i].print();
+        }
+    }
 };
